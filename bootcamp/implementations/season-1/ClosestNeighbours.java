6b3ac6991cbd488443779882b27ab8a25062e51a
JMendanha <joao.mendanha@codeforall.com>
2024-05-15 14:23:00 +0100

    solves array exercise

--- bootcamp/implementations/season-1/ClosestNeighbours.java
+++ bootcamp/implementations/season-1/ClosestNeighbours.java
@@ -9,14 +9,14 @@ class ClosestNeighbours {
         int minDifference = difference;
         int[] neighbours = {myArray[0], myArray[1]};
 
-        for (int i = 1; i < numbers.length - 1; i++) {
+        for (int i = 1; i < myArray.length - 1; i++) {
 
             difference = Math.abs((myArray[i] - myArray[i + 1]));
 
             if (difference <= minDifference) {
                 minDifference = difference;
-                neighbours[0] = numbers[i];
-                neighbours[1] = numbers[i + 1];
+                neighbours[0] = myArray[i];
+                neighbours[1] = myArray[i + 1];
             }
         }
 
